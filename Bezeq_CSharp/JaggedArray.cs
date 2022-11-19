c1d6ed84847e0a0383ce01ff4e86e8ec930fb890
Alexandra Grenader <Alexandrag@ayehu.com>
2022-11-19 19:42:26 +0200

    Add project files.

---
+++ Bezeq_CSharp/JaggedArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bezeq_CSharp
+{
+    public class JaggedArray
+    {
+        public int[][] CreateArrayAndReturn()
+        {
+            int[][] array = new int[6][];
+            array[0] = new int[] { 1, 3 };
+            array[1] = new int[] { 5, 7, 9 };
+            array[2] = new int[] { 3, 1 };
+            array[3] = new int[] { 3, 6, 7 };
+            array[4] = new int[] { 9, 7, 5 };
+            array[5] = new int[] { 3, 67 };
+            return array;
+        }
+
+        public int[][] RemoveDuplicates(int[][] array)
+        {
+            var dict = new Dictionary<string, int[]>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = array[i].OrderBy(r => r).Select(r => r).ToArray();
+                string arrayStr = string.Join(",", array[i]);
+                if (!dict.ContainsKey(arrayStr)) dict.Add(arrayStr, array[i]);
+            }
+
+            return dict.Select(g => g.Value).ToArray();
+        }
+
+
+        public void PrintArray(int[][] array)
+        {
+            foreach (var row in array)
+            {
+                foreach (var item in row)
+                {
+                    Console.Write($"{item} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Bezeq_CSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bezeq_CSharp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Task 1
+            Console.WriteLine("Input string, please :");
+            string inputStr = Console.ReadLine();
+            if (string.IsNullOrEmpty(inputStr))
+            {
+                Console.WriteLine("The input string is empty");
+            }
+            else
+            {
+                Console.WriteLine("");
+                GroupWordsInString(inputStr);
+            }
+            Console.ReadLine();
+
+            //Task 2
+            try
+            {
+                var jaggedArray = new JaggedArray();
+                var array =jaggedArray.CreateArrayAndReturn();
+                array = jaggedArray.RemoveDuplicates(array);
+                jaggedArray.PrintArray(array);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            Console.ReadLine();
+
+        }
+
+        private static void GroupWordsInString(string inputStr)
+        {
+            string[] parts = inputStr.Split(' ');
+            var list = parts?.ToList<string>();
+            list = list.Where(l => l != string.Empty).Select(l => l).ToList();
+
+            var dict = list.GroupBy(word => word).ToDictionary(g => g.Key, g => g.Count());
+            foreach (KeyValuePair<string, int> pair in dict.OrderByDescending(g => g.Value))
+            {
+                Console.WriteLine("{0}, {1}", pair.Key, pair.Value);
+            }
+        }
+    }
+}
